1a1d65a1cc5f847745b277070afd342d186633c4
DerRegenschirm <obuhovavohubo@gmail.com>
2021-01-15 21:26:25 +0200

    login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,5 @@
 from .pages.main_page import MainPage
-
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -12,3 +12,8 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def should_be_login_page(self):
+    self.should_be_login_url()
+    self.should_be_login_form()
+    self.should_be_register_form()
