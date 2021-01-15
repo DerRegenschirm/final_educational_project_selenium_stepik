715c5f7a918501ba70ae5ce30e5b10280cdbc549
DerRegenschirm <obuhovavohubo@gmail.com>
2021-01-15 21:52:53 +0200

    login page

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,5 @@
 from .base_page import BasePage
-from .locators import MainPageLocators
+from .locators import LoginPageLocatorsPageLocators
 
 class LoginPage(BasePage):
 
--- test_main_page.py
+++ test_main_page.py
@@ -2,18 +2,6 @@ from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
-
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
-
-def test_guest_can_go_to_login_page(browser):
     #    link = "http://selenium1py.pythonanywhere.com"
     #    page = MainPage(browser, link)
     #   page.open()
@@ -25,3 +13,11 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
