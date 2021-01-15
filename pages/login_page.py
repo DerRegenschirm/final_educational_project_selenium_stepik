68279d11615349938acb49a762733b36dff1bbdf
DerRegenschirm <obuhovavohubo@gmail.com>
2021-01-15 21:32:38 +0200

    login page

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,6 +3,11 @@ from .locators import MainPageLocators
 
 class LoginPage(BasePage):
 
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
         assert self.browser.current_url == MainPageLocators.LOGIN_URL, "Other login link"
--- test_main_page.py
+++ test_main_page.py
@@ -13,7 +13,9 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
 
-def should_be_login_page(self):
-    self.should_be_login_url()
-    self.should_be_login_form()
-    self.should_be_register_form()
+def test_should_be_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    page.should_be_login_page()
