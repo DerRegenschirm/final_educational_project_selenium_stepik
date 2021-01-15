9efb64ea3ce37d8d320310eb2866e78f0a9bb308
DerRegenschirm <obuhovavohubo@gmail.com>
2021-01-15 21:53:25 +0200

    login page

--- pages/login_page.py
+++ pages/login_page.py
@@ -13,7 +13,7 @@ class LoginPage(BasePage):
         assert "login" in self.browser.current_url, "Other login link"
 
     def should_be_login_form(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_FORM), "Login form is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_REGISTER_FORM), "Login registration form is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_REGISTER_FORM), "Login registration form is not presented"
