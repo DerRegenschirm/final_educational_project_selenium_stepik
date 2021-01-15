a70e3e7c912f516014e2fab331e3b0f8f2570a1f
DerRegenschirm <obuhovavohubo@gmail.com>
2021-01-15 20:50:19 +0200

    add exception message /PageObject

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,5 @@
+from selenium.common.exceptions import имя_исключения
+
 class BasePage():
     def __init__(self, browser, url):
         self.browser = browser
@@ -5,3 +7,17 @@ class BasePage():
 
     def open(self):
         self.browser.get(self.url)
+
+    # неявное ожидание
+    def __init__(self, browser, url, timeout=10):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+
+    # перехват исключения
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (имя исключения):
+            return False
+        return True
