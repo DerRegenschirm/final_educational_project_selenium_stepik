12970bef9d61b6cd0235ed18dbe41ef7282b28da
DerRegenschirm <obuhovavohubo@gmail.com>
2021-02-18 20:50:14 +0200

    element present / disappeared

--- pages/base_page.py
+++ pages/base_page.py
@@ -21,3 +21,20 @@ class BasePage():
         except (NoSuchElementException):
             return False
         return True
+
+#элемент не появляется на странице в течение заданного времени
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
+
+#элемент исчезает
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+        return True
--- pages/product_page.py
+++ pages/product_page.py
@@ -54,3 +54,11 @@ class ProductPage(BasePage):
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_not_be_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is not disappeared, but should be"
