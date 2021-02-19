856aef94c3f760773a9bc5b2ea12593e50c3f0fe
DerRegenschirm <obuhovavohubo@gmail.com>
2021-02-19 15:37:00 +0200

    add login page actions to Base Class

--- pages/base_page.py
+++ pages/base_page.py
@@ -20,7 +20,7 @@ class BasePage():
         self.browser.implicitly_wait(timeout)
 
     def go_to_login_page(self):
-        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK_INVALID)
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
         link.click()
 
     def should_be_login_link(self):
