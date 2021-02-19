041f5c047559e04c9814748d6a957b3fa08bca19
DerRegenschirm <obuhovavohubo@gmail.com>
2021-02-19 17:13:12 +0200

    basket page empty cart - no items & message

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,7 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
-
+import pytest
 
 def test_guest_can_go_to_login_page(browser):
     #    link = "http://selenium1py.pythonanywhere.com"
--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,6 @@
 from .pages.product_page import ProductPage
 from .pages.basket_page import BasketPage
-#import pytest
+import pytest
 
 #@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
 #                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
