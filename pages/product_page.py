151adb71adae292e4adaba97cf7f0e359a001680
DerRegenschirm <obuhovavohubo@gmail.com>
2021-02-19 14:46:17 +0200

    element present / disappeared

--- pages/product_page.py
+++ pages/product_page.py
@@ -59,6 +59,6 @@ class ProductPage(BasePage):
         assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
             "Success message is presented, but should not be"
 
-    def should_not_be_success_message(self):
+    def should_disappear_success_message(self):
         assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
             "Success message is not disappeared, but should be"
--- test_product_page.py
+++ test_product_page.py
@@ -23,17 +23,20 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
-    page.is_not_element_present()
+    #page.is_not_element_present()
+    page.should_not_be_success_message()
 
 def test_guest_cant_see_success_message(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209"
     page = ProductPage(browser, link)
     page.open()
-    page.is_not_element_present()
+    #page.is_not_element_present()
+    page.should_not_be_success_message()
 
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209"
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
-    page.is_disappeared()
+    #page.is_disappeared()
+    page.should_disappear_success_message()
