0252ce1873709d7cdfac102fafa6cfa39fbfa7f9
DerRegenschirm <obuhovavohubo@gmail.com>
2021-02-19 17:09:19 +0200

    basket page empty cart - no items & message

--- test_main_page.py
+++ test_main_page.py
@@ -2,7 +2,6 @@ from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 
-import pytest
 
 def test_guest_can_go_to_login_page(browser):
     #    link = "http://selenium1py.pythonanywhere.com"
@@ -23,7 +22,6 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
 
-@pytest.mark.new
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,6 @@
 from .pages.product_page import ProductPage
 from .pages.basket_page import BasketPage
-import pytest
+#import pytest
 
 #@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
 #                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -56,7 +56,6 @@ def test_guest_can_go_to_login_page_from_product_page (browser):
     page.open()
     page.go_to_login_page()
 
-@pytest.mark.new
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
