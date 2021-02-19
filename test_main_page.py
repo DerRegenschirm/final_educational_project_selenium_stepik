fa538ed1ae0a93a05d71077e84271e42b10f4b2f
DerRegenschirm <obuhovavohubo@gmail.com>
2021-02-19 16:56:40 +0200

    basket page empty cart - no items & message

--- test_main_page.py
+++ test_main_page.py
@@ -2,6 +2,8 @@ from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 
+import pytest
+
 def test_guest_can_go_to_login_page(browser):
     #    link = "http://selenium1py.pythonanywhere.com"
     #    page = MainPage(browser, link)
@@ -21,6 +23,7 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
 
+@pytest.mark.new
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
--- test_product_page.py
+++ test_product_page.py
@@ -56,6 +56,7 @@ def test_guest_can_go_to_login_page_from_product_page (browser):
     page.open()
     page.go_to_login_page()
 
+@pytest.mark.new
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
